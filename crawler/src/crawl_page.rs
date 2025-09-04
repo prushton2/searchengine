b2a7de7f051c2dc8a892b1c136e843634e63958e
prushton2 <peterrushton418@gmail.com>
2025-09-03 23:23:31 -0400

    Added blocking curl request

---
+++ crawler/src/crawl_page.rs
@@ -0,0 +1,84 @@
+use std::str;
+use curl::easy::WriteError;
+use scraper::{Html, Selector};
+use std::collections::HashMap;
+
+
+#[derive(Clone)]
+struct PageContent {
+    links: Vec<String>,
+    text: String
+}
+
+#[derive(Debug)]
+struct CrawledPage {
+    version: u32,
+    url: String,
+    words: HashMap<String, u32>
+}
+
+pub fn process_out(bytes: &[u8], url: &str, crawled_urls: &mut Vec<&str>) -> Result<usize, WriteError> {
+    
+    let page_content = strip_html(bytes).unwrap();
+
+    let crawled_page = crawl_page(page_content, url);
+
+    println!("{:?}", crawled_page);
+    
+    return Ok(bytes.len());
+}
+
+fn strip_html(bytes: &[u8]) -> Result<PageContent, &str> {
+    let mut page_content: PageContent = PageContent { links: vec![], text: String::from("") };
+
+    let html: &str = match str::from_utf8(bytes) {
+        Ok(v) => v,
+        Err(_e) => panic!("Not valid utf8")
+    };
+
+    let document = Html::parse_document(html);
+
+    let link_selector = Selector::parse("a").unwrap();
+    let body_selector = Selector::parse("body").unwrap();
+
+    // body text parsing
+    for element in document.select(&body_selector) {
+        for child in element.text() {
+            page_content.text = page_content.text + " " + child;
+        }
+    }
+
+    for element in document.select(&link_selector) {
+        let link = match element.attr("href") {
+            Some(url) => url,
+            None => ""
+        };
+
+        if link == "" {
+            continue;
+        }
+
+        page_content.links.push(String::from(link))
+    }
+    Ok(page_content)
+}
+
+fn crawl_page(page: PageContent, url: &str) -> Result<CrawledPage, &str> {
+    let mut crawled_page = CrawledPage{
+        version: 1,
+        url: String::from(url),
+        words: [].into()
+    };
+
+    for word in page.text.as_str().split(' ') {
+        let trimmed = word.trim().chars().filter(|c| c.is_alphanumeric()).collect::<String>();
+
+        if trimmed == "" {
+            continue;
+        }
+
+        crawled_page.words.entry(trimmed).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    Ok(crawled_page)
+}
--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,70 +1,41 @@
-use std::str;
-use std::collections::HashMap;
-use curl::easy::{Easy, WriteError};
-use scraper::{Html, Selector};
+use curl::easy::{Easy};
+use tokio;
 
-#[derive(Clone)]
-struct PageContent {
-    links: Vec<String>,
-    text: String
-}
+mod crawl_page;
+// mod blocking;
 
-struct CrawledPage {
-    version: String,
-    url: String,
-    words: HashMap<String, u32>
-}
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
     let url = "https://example.com/";
 
-    let mut curl = Easy::new();
-    curl.url(url).unwrap();
-    let _ = curl.write_function(|bytes| {process_out(bytes, url)}).unwrap();
-    curl.perform().unwrap();
+    let _ = crawl_and_save(url);
 }
 
-fn process_out(bytes: &[u8], url: &str) -> Result<usize, WriteError> {
-    
-    let page_content = strip_html(bytes).unwrap();
+fn crawl_and_save(url: &str) -> Vec<&str>{
 
+    let mut destinations = vec![];
+        
+    //body
+    let mut out_vec = Vec::new();
 
-    
-    return Ok(bytes.len());
-}
-
-fn strip_html(bytes: &[u8]) -> Result<PageContent, &str> {
-    let mut page_content: PageContent = PageContent { links: vec![], text: String::from("") };
-
-    let html: &str = match str::from_utf8(bytes) {
-        Ok(v) => v,
-        Err(_e) => panic!("Not valid utf8")
-    };
-
-    let document = Html::parse_document(html);
-
-    let link_selector = Selector::parse("a").unwrap();
-    let body_selector = Selector::parse("body").unwrap();
-
-    // body text parsing
-    for element in document.select(&body_selector) {
-        for child in element.text() {
-            page_content.text = page_content.text + " " + child;
-        }
+    {
+        let mut curl = Easy::new();
+        curl.url(url).unwrap();
+        let mut transfer = curl.transfer();
+        
+        let _ = transfer.write_function(|bytes| {
+            out_vec.extend_from_slice(bytes);
+            return Ok(bytes.len())
+        }).unwrap();
+        
+        transfer.perform().unwrap();
     }
 
-    for element in document.select(&link_selector) {
-        let link = match element.attr("href") {
-            Some(url) => url,
-            None => ""
-        };
+    let byte_array: &[u8] = out_vec.as_slice();
 
-        if link == "" {
-            continue;
-        }
+    let _ = crawl_page::process_out(&byte_array, url, &mut destinations);
 
-        page_content.links.push(String::from(link))
-    }
-    Ok(page_content)
+    return destinations;
 }
