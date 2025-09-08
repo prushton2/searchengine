d6602364bbd95e55dd834f72656fe9818c41e3fa
prushton2 <peterrushton418@gmail.com>
2025-09-08 19:49:04 -0400

    backend is functional

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 )
+
+type IndexedPage struct {
+	Urls [][]any `json:"urls"`
+}
+
+type ScoredURLs struct {
+	Urls map[string]float64 `json:"urls"`
+}
+
+func addScoredURLs(self ScoredURLs, other ScoredURLs) ScoredURLs {
+	for key, value := range other.Urls {
+		selfScore, exists := self.Urls[key]
+
+		if exists {
+			self.Urls[key] = value + selfScore
+		} else {
+			self.Urls[key] = value
+		}
+	}
+
+	return self
+}
 
 func search(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
-	search := query.Get("s");
-	fmt.Printf("%s", search);
+	search := strings.Split(query.Get("s"), " ")
+	fmt.Fprintf(w, "Search terms: %v\n", search)
 
-	io.WriteString(w, "This is my website!\n")
+	var scoredURLs ScoredURLs = ScoredURLs{
+		Urls: make(map[string]float64),
+	}
+
+	for _, word := range search {
+		scoredURLs = addScoredURLs(get_word_score(word), scoredURLs)
+	}
+
+	bytes, _ := json.Marshal(scoredURLs)
+
+	io.Writer.Write(w, bytes)
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+func get_word_score(word string) ScoredURLs {
+	var url = fmt.Sprintf("../indexer_data/indexed_sites/%s/%s.json", word[0:2], word)
+	contents, err := os.ReadFile(url)
+
+	if err != nil {
+		fmt.Printf("	Err: %s\n", err)
+	}
+
+	var indexedPage IndexedPage
+	err = json.Unmarshal(contents, &indexedPage)
+
+	if err != nil {
+		fmt.Printf("Err: %s\n", err)
+	}
+
+	var scoredURLs ScoredURLs = ScoredURLs{
+		Urls: make(map[string]float64),
+	}
+
+	for _, page := range indexedPage.Urls {
+		scoredURLs.Urls[page[0].(string)] = page[1].(float64)
+	}
+
+	return scoredURLs
 }
 
 func main() {
 	http.HandleFunc("/search", search)
-	http.HandleFunc("/hello", getHello)
 
+	fmt.Println("Running server")
 	_ = http.ListenAndServe(":3333", nil)
 }
