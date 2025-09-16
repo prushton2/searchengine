37af8cc6b3971b8af4bd0f8cd1b227542626c704
prushton2 <peterrushton418@gmail.com>
2025-09-16 17:43:51 -0400

    added env to frontend

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -2,6 +2,6 @@ import { type ScoredUrls } from "./models/ScoredUrl";
 import axios from "axios";
 
 export async function Search(query: string): Promise<ScoredUrls | undefined> {
-    let response = await axios.get(`http://localhost:3333/search?s=${query}`)
+    let response = await axios.get(`${import.meta.env.BACKEND_URL}/search?s=${query}`)
     return response.data as ScoredUrls;
 }
