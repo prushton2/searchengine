2a4798f9da8ae9ba5b258a63edf8a96274f29335
prushton2 <peterrushton418@gmail.com>
2025-11-10 11:26:48 -0500

    moved to main.rs

---
+++ indexer/src.bak/main.rs
@@ -0,0 +1,55 @@
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+
+mod crawled_page;
+mod indexed_page;
+mod dictionary;
+mod database;
+
+fn main() {
+    let dbinfo = database::DBInfo{
+        host: dotenv::var("POSTGRES_DB_HOST").unwrap(),
+        username: dotenv::var("POSTGRES_DB_USER").unwrap(),
+        password: dotenv::var("POSTGRES_DB_PASSWORD").unwrap(),
+        dbname: dotenv::var("POSTGRES_DB_DATABASE").unwrap(),
+    };
+
+    let environment: String = dotenv::var("ENVIRONMENT").unwrap();
+
+    let mut db = database::Database::new(&dbinfo);
+
+    loop {
+        let start = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("").as_millis();
+        if environment == "dev" { println!("Starting index..."); }
+
+        match indexer_thread(&mut db) {
+            Ok(_) =>  if environment == "dev" { println!("Index successful"); },
+            Err(t) => println!("Index failed: {}", t),
+        };
+
+        let end = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("").as_millis();
+        let dt: f64 = ((end - start) as f64) /1000.0;
+        if environment == "dev" { println!("Index took {} seconds", dt); }
+        sleep(Duration::new(20, 0));
+    }
+}
+
+fn indexer_thread(db: &mut database::Database) -> Result<&'static str, &'static str>{
+    for _i in 0..db.crawled_page_len() {
+        let mut crawledpage: crawled_page::CrawledPage = match db.get_crawled_page() {
+            Some(t) => t,
+            None => {continue;}
+        };
+
+        let _ = crawledpage.filter_stop_words();
+        
+        let indexedpage = match crawledpage.index() {
+            Ok(t) => t,
+            Err(_) => {continue;}
+        };
+
+        let _ = db.write_indexed_page(&indexedpage);
+    }
+
+    return Ok("")
+}
